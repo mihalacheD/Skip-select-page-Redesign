ced6701733bc9c1276c573046c58a2c872b5ab3e
mihalacheD <danutza_dima88@yahoo.com>
2025-03-21 20:12:17 +0200

    Install React Query & axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools }from '@tanstack/react-query-devtools'
 import { ThemeProvider } from "next-themes"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../src/styles/index.css'
 import App from './App'
 
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
    <ChakraProvider value={defaultSystem}>
      <ThemeProvider attribute="class" disableTransitionOnChange>
-      <App />
+     <QueryClientProvider client={queryClient}>
+       <App />
+       <ReactQueryDevtools/>
+     </QueryClientProvider>
     </ThemeProvider>
    </ChakraProvider>
   </StrictMode>,
