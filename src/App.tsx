e3392e079024943296c56cbbe875c49c08a0fea7
mihalacheD <danutza_dima88@yahoo.com>
2025-03-23 16:38:43 +0200

    Build the Footer

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-    <Grid templateAreas={{ base: `"nav" "main"` }} w="100%" overflowX="auto" mt={5} mb={9}>
+    <Grid templateAreas={{ base: `"nav" "main"` }} w="100%" overflowX="auto" mt={5} mb='8em'>
 
       <GridItem area="nav" overflowX="auto">
         <NavBar />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Skip } from "@/hooks/useSkips";
 import { Box, Flex, Button, Text } from "@chakra-ui/react";
+import { ArrowRight } from "lucide-react";
 
 interface Props {
   selectedSkip: Skip | null;
@@ -15,28 +16,52 @@ const Footer = ({ selectedSkip }: Props) => {
       bottom="0"
       left="0"
       w="100%"
-      bg="black"
+      bg='var(--gray)'
       color="white"
       p={4}
-      borderTop="1px solid gray"
+      borderTop='solid 0.2em'
+      borderColor='gray.800'
+      zIndex="10"
     >
       <Flex
-        direction={{ base: "column", md: "row" }}
-        justify="space-between"
+        direction={{ base: "column", lg: "row" }}
+        justify="space-around"
         align="center"
         gap={4}
       >
-        {/* Secțiunea 1: Informații Skip */}
-        <Flex direction="column" textAlign={{ base: "center", md: "left" }}>
-          <Text fontSize="lg" fontWeight="bold">{selectedSkip.size}</Text>
-          <Text fontSize="md">{selectedSkip.totalPricePerWeek} GBP</Text>
-          <Text fontSize="sm" color="gray.400">7-day hire</Text>
+
+        <Flex direction="row" gap={4} >
+          <Text fontSize="lg" fontWeight="bold" color='var(--lightgray)' >{selectedSkip.size} Yard</Text>
+          <Text fontSize='xl' fontWeight='bold' color='var(--blue)'>£{selectedSkip.totalPricePerWeek}</Text>
+          <Text fontSize='lg' color='var(--lightgray)'>{selectedSkip.hire_period_days} day hire</Text>
         </Flex>
 
-        {/* Secțiunea 2: Butoane */}
-        <Flex gap={4}>
-          <Button variant="outline" colorScheme="gray">Back</Button>
-          <Button colorScheme="blue">Continue</Button>
+        <Flex gap={4} direction='row' w="auto" justify='center' align='center'>
+          <Button
+             color='white'
+             variant="subtle"
+             borderRadius="lg"
+             bg="gray.800"
+             w={{ base: "75%", md: "100%"}}
+             _hover={{ bg: "gray.700", transform: "scale(1.05)" }}
+             _active={{ bg: "gray.600", transform: "scale(0.98)" }}
+          >
+          <Text fontSize={{ base: "md", lg: "lg"}} fontWeight='semibold'>Back</Text>
+          </Button>
+
+          <Button
+             color='white'
+             variant="subtle"
+             borderRadius="lg"
+             bg="var(--blue)"
+             w={{ base: "75%", md: "100%"}}
+             _hover={{ bg: "var(--lightblue)", transform: "scale(1.05)" }}
+             _active={{ bg: "var(--blue)", transform: "scale(0.98)" }}
+          >
+           <Text fontSize={{ base: "md", lg: "lg"}} fontWeight='semibold'>Continue</Text>
+           <ArrowRight />
+          </Button>
+
         </Flex>
       </Flex>
     </Box>
